b5e9225b8ca163cde09e4bd07c8ae91b2be995b8
laumardi <100428953@alumnos.uc3m.es>
2021-11-30 15:35:21 -0500

    Added client object

--- client.py
+++ client.py
@@ -7,20 +7,31 @@ context = ssl.create_default_context()
 global length
 
 
-def client(file_path):
-    with socket.create_connection((hostname, 8443)) as client_socket:
-        with context.wrap_socket(client_socket, server_hostname=hostname) as server_socket:
-            print(server_socket.version())
-            f = open(file_path)
-            sha256 = hashlib.sha256()
-            global length
-            length = 0
+class Client:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.hash = ''
 
-            for _ in range(10):
-                line = f.readlines()
-                length += len(line)
-                if not line:
-                    break
-                sha256.update(line)
+    def send_lines(self):
+        with socket.create_connection((hostname, 8443)) as client_socket:
+            with context.wrap_socket(client_socket, server_hostname=hostname) as server_socket:
+                print(server_socket.version())
+                f = open(self.file_path)
+                sha256 = hashlib.sha256()
+                global length
+                length = 0
 
-            client_socket.sendall(sha256.hexdigest().encode())
+                for _ in range(10):
+                    line = f.readlines()
+                    length += len(line)
+                    if not line:
+                        break
+                    sha256.update(line)
+
+                self.hash = sha256.hexdigest()
+                client_socket.sendall(self.hash.encode())
+
+    def compare(self, other_hash):
+        if self.hash == other_hash:
+            return True
+        return False
